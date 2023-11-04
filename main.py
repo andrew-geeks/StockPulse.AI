4ee069207a507c02e1b0105ed5b46e1aa7379bb0
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-11-04 22:46:11 +0530

    main

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st 
 import pandas as pd
+import os
+from dotenv import load_dotenv
+import pickle
 
+load_dotenv()
+
+api_key = os.environ['API_KEY'] #api_key for marketaux
+
+st.set_page_config(page_title="StockPulse.AI📈🤖")
 st.title("StockPulse.AI📈🤖")
 st.text("Data app to analyze the sentiment of Indian Stock market!")
+
+
 
