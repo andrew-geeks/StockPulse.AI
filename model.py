50d1555aa81ff3fee4294274af26f01d63b43ee9
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-11-07 23:28:39 +0530

    main

---
+++ model.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+model = AutoModelForSequenceClassification.from_pretrained("KernAI/stock-news-distilbert")
+
+tokenizer = AutoTokenizer.from_pretrained("KernAI/stock-news-distilbert")
+
+classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+result = classifier("Tata Motors gives up early gains despite deal to pick up 27% in Freight Tiger")
+print(result)
