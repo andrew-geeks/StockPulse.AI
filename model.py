9d77841f134ed7dad3047e1a0753fcff24eaf6fb
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-11-14 10:21:39 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,4 +7,5 @@ tokenizer = AutoTokenizer.from_pretrained("KernAI/stock-news-distilbert")
 
 classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
 result = classifier("Tata Motors gives up early gains despite deal to pick up 27% in Freight Tiger")
+
 print(result)
