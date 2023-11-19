8484e335b59ecf239f08c27b119786ab951a2c0d
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-11-19 21:36:57 +0530

    main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import requests
 from dotenv import load_dotenv
 
-import pickle
+import json
 
 from transformers import pipeline
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
@@ -14,7 +14,7 @@ model = AutoModelForSequenceClassification.from_pretrained("KernAI/stock-news-di
 tokenizer = AutoTokenizer.from_pretrained("KernAI/stock-news-distilbert")
 
 classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
-result = classifier("Global Digital MRO Market Size To Worth USD 3396 Million By 2032 | CAGR of 12.4%")
+#result = classifier("Global Digital MRO Market Size To Worth USD 3396 Million By 2032 | CAGR of 12.4%")
 
 
 load_dotenv()
@@ -25,10 +25,14 @@ def mainSentiment():
     sentiment_label = []
     sentiment_score = []
     
-    data = requests.get("https://api.marketaux.com/v1/news/all?filter_entities=true&language=en&countries=in&industries=&api_token="+api_key).json
+    data = requests.get("https://api.marketaux.com/v1/news/all?filter_entities=true&language=en&countries=in&industries=&api_token="+api_key).json()
+    print(type(data))
+    for i in range(0,len(data['data'])-1):
+        label = classifier(data['data'][i]['title'])[0]['label']
+        sentiment_label.append(label)
     
-    
-    return data.json["data"]
+    st.text(sentiment_label)
+        
 
 st.set_page_config(page_title="StockPulse.AI📈🤖")
 st.title("StockPulse.AI📈🤖")
